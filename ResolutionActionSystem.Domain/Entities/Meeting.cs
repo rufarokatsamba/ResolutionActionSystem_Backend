d0a84bcf2e04bce05b80657e72b868d717839263
Rufaro Katsamba <rufaro@velocityinc.tech>
2022-11-09 15:00:46 +0200

    added domain entities relationships

---
+++ ResolutionActionSystem.Domain/Entities/Meeting.cs
@@ -0,0 +1,17 @@
+﻿using ResolutionActionSystem.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolutionActionSystem.Domain.Entities
+{
+    public class Meeting: BaseDomainEntity
+    {
+        public string? Description { get; set; }
+        public MeetingType MeetingType { get; set; }
+        public int MeetingTypeId { get; set; }
+        public List<MeetingItem> MeetingItems { get; set; }
+    }
+}
---
+++ ResolutionActionSystem.Domain/Entities/MeetingItem.cs
@@ -0,0 +1,21 @@
+﻿using ResolutionActionSystem.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolutionActionSystem.Domain.Entities
+{
+    public class MeetingItem : BaseDomainEntity
+    {
+        public DateTime DueDate { get; set; }
+        public string? PersonResponsible { get; set; }
+        public string? ItemComment { get; set; }
+        public string? Action { get; set; }
+        public string? Status { get; set; }
+        public bool? isClosed { get; set; }
+        public Meeting Meeting { get; set; }
+        public int MeetingId { get; set; }
+    }
+}
